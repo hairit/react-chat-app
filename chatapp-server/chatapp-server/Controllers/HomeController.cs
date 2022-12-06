36708c0f42c1f98547571f5d0518da91deaa6890
Marcus Pham <mpham@paritycorp.com>
2022-12-06 20:40:52 +0700

    Init project

---
+++ chatapp-server/chatapp-server/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace chatapp_server.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class HomeController : ControllerBase
+    {
+        [HttpGet]
+        public string run()
+        {
+            return "Server is running on port:";
+        }
+    }
+}
